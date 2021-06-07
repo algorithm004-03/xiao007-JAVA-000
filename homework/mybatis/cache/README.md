ad449da0cfb65c83411a5ddaa25bd044c4091ffe
萧 <1243925457@qq.com>
2021-06-07 22:59:43 +0800

    mybatis plus 学习

---
+++ homework/mybatis/cache/README.md
@@ -0,0 +1,6 @@
+# Mybatis plus Cache 探索
+***
+
+## 参考链接
+- [junit4 maven install](https://github.com/junit-team/junit4/wiki/Download-and-Install)
+- [Mybatis plus 官方中文快速入门](https://baomidou.com/guide/quick-start.html#%E5%BC%80%E5%A7%8B%E4%BD%BF%E7%94%A8)
---
+++ homework/mybatis/cache/src/test/java/com/example/cache/UserTest.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.cache;
+
+import com.example.cache.entity.User;
+import com.example.cache.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+/**
+ * @author liuwei
+ */
+@SpringBootTest
+@Slf4j
+public class UserTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelectOneCache() {
+        List<User> users = userMapper.selectList(null);
+        Assertions.assertEquals(5, users.size());
+        users.forEach(System.out::println);
+
+        users = userMapper.selectList(null);
+        Assertions.assertEquals(5, users.size());
+        users.forEach(System.out::println);
+    }
+}
