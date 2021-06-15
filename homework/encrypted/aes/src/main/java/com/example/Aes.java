94252a816abd4868ad83f245ae7b799840992c39
萧 <1243925457@qq.com>
2021-06-16 07:27:46 +0800

    增加加解密示例

---
+++ homework/encrypted/aes/src/main/java/com/example/Aes.java
@@ -0,0 +1,102 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import java.security.Key;
+import java.security.SecureRandom;
+
+/**
+ * reference link: https://blog.csdn.net/u011781521/article/details/77932321
+ * @author liuwei
+ */
+public class Aes {
+
+    /**
+     * 根据密钥对指定的明文plainText进行加密.
+     *
+     * @param plainText 明文
+     * @return 加密后的密文.
+     */
+    public static String encrypt(String plainText) {
+        Key secretKey = getKey("fendo888");
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte[] p = plainText.getBytes("UTF-8");
+            byte[] result = cipher.doFinal(p);
+            BASE64Encoder encoder = new BASE64Encoder();
+            String encoded = encoder.encode(result);
+            return encoded;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 根据密钥对指定的密文cipherText进行解密.
+     *
+     * @param cipherText 密文
+     * @return 解密后的明文.
+     */
+    public static String decrypt(String cipherText) {
+        Key secretKey = getKey("fendo888");
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            BASE64Decoder decoder = new BASE64Decoder();
+            byte[] c = decoder.decodeBuffer(cipherText);
+            byte[] result = cipher.doFinal(c);
+            return new String(result, "UTF-8");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Key getKey(String keySeed) {
+        if (keySeed == null) {
+            keySeed = System.getenv("AES_SYS_KEY");
+        }
+        if (keySeed == null) {
+            keySeed = System.getProperty("AES_SYS_KEY");
+        }
+        if (keySeed == null || keySeed.trim().length() == 0) {
+            keySeed = "abcd1234!@#$";// 默认种子
+        }
+        try {
+            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+            secureRandom.setSeed(keySeed.getBytes());
+            KeyGenerator generator = KeyGenerator.getInstance("AES");
+            generator.init(secureRandom);
+            return generator.generateKey();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        String encodeStr = encrypt("1;2;3");
+        System.out.println(encodeStr);
+        String decodeStr = decrypt(encodeStr);
+        System.out.println(decodeStr);
+    }
+}
