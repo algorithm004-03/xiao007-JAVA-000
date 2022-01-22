72b570dc6d6dc560bb10cd9a8bb6874607662578
萧 <1243925457@qq.com>
2022-01-22 09:01:01 +0800

    shangdingdb init

---
+++ code/shardingsphere/shardingdb/src/main/java/com/shardingsphere/shardingdb/ShardingdbApplication.java
@@ -0,0 +1,13 @@
+package com.shardingsphere.shardingdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingdbApplication.class, args);
+    }
+
+}
